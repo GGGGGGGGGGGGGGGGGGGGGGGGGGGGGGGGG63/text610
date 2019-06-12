9d0e261c014cdead0cf1492fcf0c70eb8a12c26e
s20181105880 <s20181105880@admindeiMac-62.local>
2019-06-12 21:30:13 +0800

    Initial Commit

---
+++ text610/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text610
+//
+//  Created by s20181105880 on 2019/6/12.
+//  Copyright © 2019 s20181105880. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
